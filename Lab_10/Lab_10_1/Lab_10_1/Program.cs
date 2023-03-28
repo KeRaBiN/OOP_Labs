7dec222f97697113fb2dd365da04f4aba8d4ebe6
KeRaBiN <kuzmat278@gmail.com>
2023-03-28 18:13:56 +0300

    Complete Lab 10

---
+++ Lab_10/Lab_10_1/Lab_10_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_10_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> Arr = new List<int>();
+            Random rnd = new Random();
+            int Result = 0;
+
+            for (int i = 0; i < 20; i++)
+            {
+                Arr.Add(rnd.Next(-10, 11));
+                Console.Write(Arr[i] + " ");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0, j = 0; i < 20 && j != 4; i++)
+            {
+                if (Arr[i] < 0)
+                {
+                    j++;
+                    Result += Arr[i];
+                }
+            }
+            Console.WriteLine("Sum of the first 4 negative numbers: " + Result);
+        }
+    }
+}
---
+++ Lab_10/Lab_10_2/Lab_10_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_10_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Arr = new List<int>();
+            Random rnd = new Random();
+            int Result = 1;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Arr.Add(rnd.Next(-20, 21));
+                Console.Write(Arr[i] + " ");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < 10; i++)
+                if (Math.Abs(Arr[i]) > 10)
+                    Result *= Arr[i];
+
+            Console.WriteLine("Multiplication of all numbers that higher then 10 or lower then -10 is: " + Result);
+        }
+    }
+}
