52274a8744df2aa77242d04f0e0e1ea929a50b01
KeRaBiN <kuzmat278@gmail.com>
2023-03-28 17:08:04 +0300

    Complete Lab 9

---
+++ Lab_9/Lab_9_1/Lab_9_1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_9_1
+{
+    class Program
+    {
+        static double CalcFormula(double x, double y, double z)
+        {
+            return Math.Pow(x, 2) * Math.Pow((Math.Pow(z, 2) + Math.Abs(y)), 0.25);
+        }
+
+        static void Main(string[] args)
+        {
+            double X = 1, Y = 1, Z = 1, A = 1;
+
+            while (X != 0)
+            {
+                Console.Write("Type x: ");
+                X = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Type y: ");
+                Y = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Type z: ");
+                Z = Convert.ToInt32(Console.ReadLine());
+
+                A = CalcFormula(X, Y, Z);
+                Console.WriteLine("\nResult:\n a = " + A);
+
+                if (X > 1)
+                {
+                    if (X > 10)
+                        Console.WriteLine("X higher then 10");
+                    else
+                        Console.WriteLine("X higher then 1");
+                }
+                else if (X < -1)
+                {
+                    if (X < -10)
+                        Console.WriteLine("X lower then -10");
+                    else
+                        Console.WriteLine("X lower then -1");
+                }
+
+                if (A < 0)
+                {
+                    Console.WriteLine("A is negative");
+                }
+                else
+                {
+                    Console.WriteLine("A is positive");
+                }
+
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
---
+++ Lab_9/Lab_9_2/Lab_9_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_9_1
+{
+    class Program
+    {
+        static long CalcSeconds(int days, int weeks, int years)
+        {
+            return ((long)86400) * (days + (weeks * 7) + (years * 365));
+        }
+
+        static void Main(string[] args)
+        {
+            int Days = 0, Weeks = 0, Years = 0;
+            long Seconds = 0;
+
+            Console.Write("Enter amount of days: ");
+            Days = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter amount of weeks: ");
+            Weeks = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter amount of years: ");
+            Years = Convert.ToInt32(Console.ReadLine());
+
+            Seconds = CalcSeconds(Days, Weeks, Years);
+
+            Console.WriteLine("\nIn " + Days + " days, " + Weeks + " weeks, " + Years + " years, there are " + Seconds + " seconds");
+
+            Console.WriteLine();
+
+        }
+    }
+}
