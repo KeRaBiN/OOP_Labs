aa20701fd10ee995b205ceb32a9339d33e1a4fe8
KeRaBiN <kuzmat278@gmail.com>
2023-03-28 19:31:51 +0300

    Complete Lab 11

---
+++ Lab_11/Lab_11/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_11
+{
+    class Program
+    {
+        static string EncriptString(string line)
+        {
+            string result = "";
+            for (int i = 0; i < line.Length; i++)
+                result += (Convert.ToInt32(line[i]) + ".");
+            result += '/';
+            return result;
+        }
+        static string DecriptString(string line)
+        {
+            string result = "";
+            char symbol = '0';
+            string code = "";
+            for (int i = 0; symbol != '/'; i++)
+            {
+                symbol = line[i];
+                if (symbol == '.')
+                {
+                    result += Convert.ToChar(Convert.ToInt32(code));
+                    code = "";
+                }
+                else
+                    code += symbol;
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            string Name, Surname;
+
+            Console.Write("Type name: ");
+            Name = Console.ReadLine();
+            Console.Write("Type surname: ");
+            Surname = Console.ReadLine();
+
+            Console.WriteLine("\nName - " + Name + ". Amount of letters in surname - " + Surname.Length + "\n");
+
+            string Text;
+
+            Console.Write("Type text: ");
+            Text = Console.ReadLine();
+
+            string encripted_str = EncriptString(Text);
+
+            Console.WriteLine("Here is encrypted text: " + encripted_str);
+            Console.WriteLine("and here is decripted: " + DecriptString(encripted_str));
+        }
+    }
+}
